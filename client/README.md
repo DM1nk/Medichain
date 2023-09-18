9bbfbab5bcf5eab0eaee2dbc01cfad854332a8e8
M1nk <Tranducminhktvn@gmail.com.vn>
2023-09-18 16:34:52 +0700

    Add client file

---
+++ client/README.md
@@ -0,0 +1,24 @@
+# Medichain for Hackyouth 2023
+A blockchain-based system for managing prescription medications
+
+# Packages: 
+
+- react
+- react-router-dom
+- react-hook-form
+- react-helmet-async
+- react-redux
+- redux
+- react-toastify
+- firebase
+- framer-motion
+- swiper
+- yup
+- tailwindcss
+- prettier
+
+# How to run
+
+1. Clone this project
+2. Install packages (npm install or yarn install)
+3. Run the project (npm start)
---
+++ client/src/context/TransactionContext.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useEffect } from 'react'
+
+import { useState } from 'react'
+import { ethers } from 'ethers'
+import { contractABI, contractAddress } from '../utils/constants'
+export const TransactionContext = React.createContext()
+
+const { ethereum } = window
+//ethereum object
+
+const getEthereumOCntract = () => {
+  const provider = new ethers.providers.Web3Provider(ethereum)
+  const signer = provider.getSigner()
+  const transactionContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  )
+
+  console.log({
+    provider,
+    signer,
+    transactionContract
+  })
+  return contract
+}
+
+export const TransactionProvider = ({ children }) => {
+  return (
+    <TransactionContext.Provider value={{value:'test'}}>
+      {children}
+    </TransactionContext.Provider>
+  );
+}
